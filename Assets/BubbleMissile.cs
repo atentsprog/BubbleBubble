7c342425a5280370081d7756ab608ba32ed98a3a
hktrollgames <hktrollgames@gmail.com>
2021-05-25 11:50:51 +0900

    적 만들기전

---
+++ Assets/BubbleMissile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 버블 이동 방식
+///     버블은 일부 벽을 통과함.
+/// 이동이 끝나면 맵마다 끌어당겨지는 지점으로 이동
+/// 
+/// todo : 몬스터가 닿을시 몬스터 버블에 갖히게 하기
+/// </summary>
+public class BubbleMissile : MonoBehaviour
+{
+    new public Rigidbody2D rigidbody2D;
+    new public Collider2D collider2D;
+    public float gravityScale = -0.5f;
+    IEnumerator Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<Collider2D>();
+        rigidbody2D.gravityScale = 0;
+
+        for (int i = 0; i < moveFrame; i++)
+        {
+            transform.Translate(speed, 0, 0);
+            yield return null;
+        }
+        rigidbody2D.gravityScale = gravityScale;
+
+        // 중력장을 찾아 이동하자. -> 맵마다 달랐음.
+        // 중력장이 1개 이상인 곳도 있었음.
+        //// 가장가까운 중력장을 찾자. 
+        //// 중력장을 바라보자
+        /// 중력장이 밑에 있으면 그래비티를 양수로 바꾸자.
+        /// 앞방향으로 힘을 가해서 이동시키자.
+
+    }
+
+    public float speed = 1;
+    public int moveFrame = 4;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision + "부딛힘");
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,43 @@ public class Player : MonoBehaviour
 {
     public float speed = 0.1f;
 
+    new public Collider2D collider;
+    new public Rigidbody2D rigidbody2D;
     public Animator animator;
+    public SpriteRenderer spriteRenderer;
     private void Start()
     {
+        Application.targetFrameRate = 60;
+        collider = GetComponent<Collider2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+    public bool isGround = true;
+    public float jumpForce = 10;
+
+    float previousY;
     private void Update()
+    {
+        FireBubble();
+
+        Jump();
+
+        Move();
+    }
+
+    public GameObject bubbleGo;
+    private void FireBubble()
+    {
+        //스페이스키 누르면 앞으로 버블 발사.
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bubbleGo, transform.position, transform.rotation);
+        }
+    }
+
+    private void Move()
     {
         // AD, A왼쪽, D오른쪽
         float moveX = 0;
@@ -19,15 +51,36 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) moveX = -1;
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) moveX = 1;
         Vector3 position = transform.position;
-        position.x = position.x + moveX * speed * Time.deltaTime;
+        position.x = position.x + moveX * speed;
         transform.position = position;
 
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("attack") == false)
         {
             if (moveX != 0)
+            {
                 animator.Play("run");
+                var rotate = transform.rotation;
+                rotate.y = moveX > 0 ? 180 : 0;
+                transform.rotation = rotate;
+                //spriteRenderer.flipX = moveX > 0;
+            }
             else
                 animator.Play("idle");
+        }
+    }
+
+    private void Jump()
+    {
+        if (previousY > transform.position.y)
+            collider.isTrigger = false;
+        previousY = transform.position.y;
+
+        // 점프
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+            collider.isTrigger = true;
+            isGround = false;
         }
     }
 }
