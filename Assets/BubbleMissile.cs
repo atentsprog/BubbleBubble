ae2991c69eb9ed310c1965b4f612ecf6a2acb857
hktrollgames <hktrollgames@gmail.com>
2021-05-25 14:26:25 +0900

    버블 미사일 작성중

--- Assets/BubbleMissile.cs
+++ Assets/BubbleMissile.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,11 +12,13 @@ using UnityEngine;
 /// </summary>
 public class BubbleMissile : MonoBehaviour
 {
+    public Animator animator;
     new public Rigidbody2D rigidbody2D;
     new public Collider2D collider2D;
     public float gravityScale = -0.5f;
     IEnumerator Start()
     {
+        animator = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
         collider2D = GetComponent<Collider2D>();
         rigidbody2D.gravityScale = 0;
@@ -39,8 +42,55 @@ public class BubbleMissile : MonoBehaviour
     public float speed = 1;
     public int moveFrame = 4;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(collision + "부딛힘");
+        Debug.Log(other.name + " Trigger 부딛힘");
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.transform.name + " Collision 부딛힘");
+
+        if (state == State.Normal)
+        {
+            if (other.transform.tag == "Enemy")
+            {
+                state = BubbleMissile.State.Capture;
+
+                //적을 감추고
+                //풍선이미지를 바꾸자.
+                // 풍선 현재 타입을 캡쳐로 설정.
+                other.transform.SetParent(transform);
+                other.transform.localPosition = Vector3.zero;
+                other.gameObject.SetActive(false);
+                caughtTarget = other.gameObject;
+
+                // 갖힌 적 타입에 따라 플레이 해야하는 애니메이션 이름 구하자.(지금은 하드 코딩)
+                StartCoroutine(BubbleExplosionTimerCo("EnemyA"));
+            }
+        }
+    }
+    public GameObject caughtTarget; //catch잡은 겟
+
+    public int[] explosionTimer;
+    private IEnumerator BubbleExplosionTimerCo(string prefix)
+    {
+        for (int i = 1; i <= explosionTimer.Length; i++)
+        {
+            animator.Play(prefix + i);
+            yield return new WaitForSeconds(explosionTimer[i]);
+        }
+
+        // 몬스터 해방.
+        caughtTarget.transform.SetParent(null);
+        caughtTarget.SetActive(true);
+    }
+
+    public State state = BubbleMissile.State.Normal;
+
+    public enum State
+    {
+        Normal,
+        Capture,
     }
 }
