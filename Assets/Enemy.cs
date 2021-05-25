e2b96dc1144ed89e873fba46068ab4f81a1ffac3
hktrollgames <hktrollgames@gmail.com>
2021-05-25 13:58:27 +0900

    적 추가

---
+++ Assets/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{    
+    new public Collider2D collider;
+    new public Rigidbody2D rigidbody;
+    public float gravitiScale = 1;
+
+    public float speed = 0.1f;
+    IEnumerator Start()
+    {
+        collider = GetComponent<Collider2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        // 지형 만날때까지 아래로 떨어지자.
+
+        Collider2D[] collider2Ds = new Collider2D[4];
+        ContactFilter2D contactFilter2D = new ContactFilter2D();
+        int collidCount = 0;
+        while (collidCount == 0)
+        {
+            collidCount = rigidbody.OverlapCollider(contactFilter2D, collider2Ds);
+            yield return null;
+        }
+        Debug.Log($"충돌한 충돌체 수 :{collider2Ds.Length}");
+
+
+        // 무한히 좌우를 왔다 갔다하자.
+        // todo:가끔 절벽쪽으로 점프 해야함.
+        //오른쪽부터 이동하자. 끝에 땋을때까지 이동하자.
+        while (true)
+        {
+            transform.Translate(speed, 0, 0);
+            yield return null;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        bool rightDirection = false;
+        if (transform.position.x > collision.transform.position.x)
+            rightDirection = true;
+
+        //충돌체 방향에 따라 로테이션을 180도 돌려주자.
+        var rotation = transform.rotation;
+        rotation.y = rightDirection ? 180f : 0;
+        transform.rotation = rotation;
+    }
+}
