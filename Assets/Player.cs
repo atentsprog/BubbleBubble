a23347b30a82ae5e9e9eba372d7721e35c2cc5a7
hktrollgames <hktrollgames@gmail.com>
2021-05-25 10:19:51 +0900

    보글보글 작업중

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    public Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        // AD, A왼쪽, D오른쪽
+        float moveX = 0;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) moveX = -1;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) moveX = 1;
+        Vector3 position = transform.position;
+        position.x = position.x + moveX * speed * Time.deltaTime;
+        transform.position = position;
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("attack") == false)
+        {
+            if (moveX != 0)
+                animator.Play("run");
+            else
+                animator.Play("idle");
+        }
+    }
+}
