b804790b5cfe19e2931814ca669f9afa2afe6250
hktrollgames <hktrollgames@gmail.com>
2021-05-27 13:34:28 +0900

    적이 유저를 공격할때 컬라이더로 판단할 것인가? 거리로 판단할 것인가?

---
+++ Assets/AttackTypeSample/AttackByRange.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AttackByRange : MonoBehaviour
+{
+    [System.Serializable]
+    public class AttackInfo
+    {
+        public string attackName = "Attack";
+        public float range = 3;
+    }
+    public List<AttackInfo> attacks;
+
+
+    public Transform player;
+
+    public float searchInterval = 1f;
+    public float attackableDistance = 6f;
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+            if (distance < attackableDistance)
+            {
+                // 랜덤으로 공격할것을 정하던가, 공격 사정거리 안에 있는 공격 종류만 선택하던가 로직으로 선택하자
+                var currentAttack = attacks[Random.Range(0, attacks.Count)]; 
+
+                if(currentAttack.range > distance)
+                    Debug.Log($"{currentAttack.attackName} 공격을 바로 하자");
+                else
+                    Debug.Log($"적 근처로 이동한다음 {currentAttack.attackName} 공격을 하자");
+            }
+            yield return new WaitForSeconds(searchInterval);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        for (int i = 0; i < attacks.Count; i++)
+        {
+            var item = attacks[i];
+            Gizmos.DrawWireSphere(transform.position, item.range);
+        }
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, attackableDistance);
+        
+    }
+}
